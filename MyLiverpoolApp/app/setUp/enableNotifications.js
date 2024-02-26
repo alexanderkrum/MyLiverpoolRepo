02f618a750f0cb0cdc4a5b2ce23b4f88bef17f2b
Dilara Ajaj <da065@uowmail.edu.au>
2024-02-26 23:03:48 +1100

    Creating the enable location feature in the setup

---
+++ MyLiverpoolApp/app/setUp/enableNotifications.js
@@ -0,0 +1,11 @@
+import {View, Text, StyleSheet} from 'react-native';
+
+function EnableNotifications() {
+    return (
+        <View>
+            <Text>Access granted!</Text>
+        </View>
+    )
+}
+
+export default EnableNotifications;
